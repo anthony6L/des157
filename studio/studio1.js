3a963ddab170df163dd27505c670eb86df06b104
anthony6L <windy@AnthonydeMacBook-Pro.local>
2018-01-28 19:23:25 -0800

    add studio 1 *doesn't work

---
+++ studio/studio1.js
@@ -0,0 +1,26 @@
+
+var results = document.getElementById("results");
+var userName = document.f.userName.value;
+var town = document.f.town.value;
+var skill = document.f.skill.value;
+var myMsg=document.getElementById("myMsg");
+
+document.f.onsubmit = processForm;
+document.f.onreset = resetForm;
+
+function processForm(){
+	results.className = "show";
+	console.log("here");
+
+	myMsg.innerHTML="Hello " + userName + " of " + town +
+					", Welcome to the Clade! You've been assigned to be a " + 
+					skill + ". Hope you will get used to the new community soon.";
+
+     //prevent page from reloading
+    return false;
+}
+
+function resetForm() {
+	myMsg.innerHTML = "";
+  	results.className = "hide";
+}
