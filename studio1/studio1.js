7f006c281d2b4fbe2fb92d4cfc3671d29ea268e7
Anthony Liu <anthony6@scc2102-11.ucdavis.edu>
2018-02-01 14:19:32 -0800

    changing file structure

---
+++ studio1/studio1.js
@@ -0,0 +1,27 @@
+var results = document.getElementById("results");
+var myMsg = document.getElementById("myMsg");
+
+document.f.onsubmit = processForm;
+document.f.onreset = resetForm;
+
+function processForm() {
+  results.className = "show";
+  var userName = document.f.userName.value;
+  var town = document.f.town.value;
+  var skill = document.f.skill.value;
+
+  myMsg.innerHTML = "Hello " + userName + " of " + town +
+    ", Welcome to the Clade! You've been assigned to be a " +
+    skill + ". Hope you will get used to the new community soon.";
+
+  document.body.style.backgroundImage = "url('images/village.jpg')";
+
+  //prevent page from reloading
+  return false;
+}
+
+function resetForm() {
+  myMsg.innerHTML = "";
+  results.className = "hide";
+  document.body.style.backgroundImage = "";
+}
