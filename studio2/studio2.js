e77274efa5b08b8349475d1a116bff064981e1ee
Anthony Liu <anthony6@scc2102-11.ucdavis.edu>
2018-02-01 16:25:37 -0800

    started studio2

---
+++ studio2/studio2.js
@@ -0,0 +1,8 @@
+"use strict";
+var laidOut = document.getElementById('laidOut');
+
+laidOut.addEventListener('click', function() {
+  laidOut.classList.add("fadeInClass");
+  laidOut.style.opacity = 1;
+
+});
